8e82dcc184aab0424ac1ca1256e44615515c606a
lszsrd <laszlo.serdet@epitech.eu>
2024-10-26 23:40:39 +0200

    [+] Added which-key

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,11 +11,18 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes"
 vim.opt.scrolloff = 8
+vim.opt.listchars = 'trail:·,nbsp:◇,tab:→ ,extends:▸,precedes:◂,eol:⁋'
 
 vim.opt.showmode = false
 
+vim.opt.mouse = 'a'
+
 -- File edition settings
 vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.cindent = true
+vim.opt.softtabstop = -1
 vim.opt.autoread = true
 
 -- Synchronise clipboard between Darwin and Neovim

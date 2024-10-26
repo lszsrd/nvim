97f922a375bc8dcf11048413f80c29f5550c03c9
lszsrd <laszlo.serdet@epitech.eu>
2024-10-27 00:02:26 +0200

    [+] Updated telescope mapping

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -17,7 +17,7 @@ return
 	    }
 	})
 
-
+    vim.keymap.set('n', "<leader>ff", ":Telescope find_files<CR>")
     vim.keymap.set('n', "<leader>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
-    end
+      end
 }
